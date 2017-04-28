57967e140e066def7ab19c887a2b0d320f74ca85
Keenuts <nathan.gauer@lse.epita.fr>
2017-04-28 15:52:48 +0200

    gitignore: added setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,4 @@
-export VULKAN_SDK=~/Projects/VulkanSDK/1.0.46.0/x86_64
+export VULKAN_SDK=~/Projects/VulkanSDK/1.0.42.0/x86_64
 export PATH=${PATH}:$VULKAN_SDK/bin
 export LD_LIBRARY_PATH=$VULKAN_SDK/lib
 export VK_LAYER_PATH=$VULKAN_SDK/etc/explicit_layer.d
